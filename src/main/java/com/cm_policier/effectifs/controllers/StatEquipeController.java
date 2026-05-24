2a2fbe5b722983e02e026211849947d1fb550e57
mukhut <mukhutjp@gmail.com>
2026-05-24 17:04:40 +0100

    modif

--- src/main/java/com/cm_policier/effectifs/controllers/StatEquipeController.java
+++ src/main/java/com/cm_policier/effectifs/controllers/StatEquipeController.java
@@ -4,12 +4,14 @@ import com.cm_policier.effectifs.dto.StatEquipeDto;
 import com.cm_policier.effectifs.service.StatEquipeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class StatEquipeController {
 
     private final StatEquipeService statEquipeService;
--- src/main/java/com/cm_policier/effectifs/controllers/StatMissionController.java
+++ src/main/java/com/cm_policier/effectifs/controllers/StatMissionController.java
@@ -5,12 +5,14 @@ import com.cm_policier.effectifs.dto.StatMissionDto;
 import com.cm_policier.effectifs.service.StatMissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class StatMissionController {
 
     private final StatMissionService statMissionService;
