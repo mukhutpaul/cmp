acaeb6c9d6236bf0f5d8799046b4f6d7e701e913
mukhut <mukhutjp@gmail.com>
2026-06-10 21:11:19 +0100

    mod

--- src/main/java/com/cm_policier/effectifs/security/SecurityConfig.java
+++ src/main/java/com/cm_policier/effectifs/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/api/pc/sync",
-                                "/login-distant",
+                                "/api/auth/login-distant",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html")
