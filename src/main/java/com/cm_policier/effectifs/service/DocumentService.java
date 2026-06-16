97de562bf4cc0c1eae3528c3cde949910ba4e77d
mukhut <mukhutjp@gmail.com>
2026-06-16 16:56:17 +0100

    modif

--- src/main/java/com/cm_policier/effectifs/service/DocumentService.java
+++ src/main/java/com/cm_policier/effectifs/service/DocumentService.java
@@ -8,12 +8,7 @@ import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
 import com.cm_policier.effectifs.dto.DocumentSyncDTO;
 import com.cm_policier.effectifs.model.Controle;
 import com.cm_policier.effectifs.model.Document;
